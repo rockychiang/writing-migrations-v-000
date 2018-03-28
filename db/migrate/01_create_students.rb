ba29a82cf1a963149242e9cf5e2beaa74a5b7e87
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-28 21:36:49 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-  end
-  create_table :students do |t|
-    t.string :name
+    create_table :students do |t|
+      t.string :name
+    end
   end
 end
